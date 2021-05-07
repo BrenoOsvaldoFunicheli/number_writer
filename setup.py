4da70cd78bd1e487bd5ecebb25a66b3c6b1808f5
Breno Osvaldo <7664249-bosvaldo@users.noreply.gitlab.com>
2021-05-07 00:13:18 -0300

    setup

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="number_writer", # Replace with your own username
-    version="0.0.1",
-    author="Example Author",
+    version="0.0.2",
+    author="Breno Osvaldo",
     author_email="bfunicheli@estudante.ufscar.com",
-    description="A small example package to transform number into text",
+    description="A small package to transform number into text",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/BrenoOsvaldoFunicheli/number_writer",
     
     classifiers=[
         "Programming Language :: Python :: 3",
